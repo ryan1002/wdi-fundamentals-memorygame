54770b7e0418b4363211534e765f42aa31448590
ryan modeste <ryan modeste>
2016-07-15 13:45:28 +0100

    added else if statement to main.js file, rearranged variables

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,11 +1,19 @@
 console.log("JS file is connected to HTML! Woo!")
 
 var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
+var cardTwo = "king";
+var cardThree = "queen";
 var cardFour = "king";
 
 /* 
 
 var cardOne = "queen";
 */
+
+if (cardTwo == cardFour) {
+	alert('You found a match');
+}
+else {
+	alert('Sorry, try again')	
+}
+	
