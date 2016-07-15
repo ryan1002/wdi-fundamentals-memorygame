6b505dbe20058f6783b6ab3c6f262b232e1fa4e3
ryan modeste <ryan modeste>
2016-07-15 09:34:01 +0100

    all memory game files linked, declared variables for king and queen

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,11 @@
 console.log("JS file is connected to HTML! Woo!")
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+/* 
+
+var cardOne = "queen";
+*/
