f4475902c9cf52c1f4db6a21825aef29643086bd
ryan modeste <ryan modeste>
2016-07-25 14:26:49 +0100

    Created function will create board, and for loop changes

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,19 +5,32 @@ var cards = ['queen', 'queen', 'king', 'king']
 
 var cardsInPlay = [];
 
-var cardOne = "queen";
-
-
 /*if (cards[0] === cards[1]) {
 	alert('You found a match');
 }
 else {
 	alert('Sorry, try again')	
 };*/
-	
-var gameBoard = document.getElementById('game-board');
 
-var createBoard = function(numCards) {
+var board = document.getElementById('game-board');
+// function will create board
+function createBoard() {
+
+for(var i=0; i<cards.length; i++){
+	
+	var newCard = document.createElement('div');
+	
+	newCard.className = 'card';
+	board.appendChild(newCard);
+		
+	}
+};
+
+creatBoard();
+
+
+
+/*var createBoard = function(numCards) {
 	for(var i=1; i<= numCards; i++) {
 	var newCard = document.getElementById('div');
 	newCard.className = "card";
