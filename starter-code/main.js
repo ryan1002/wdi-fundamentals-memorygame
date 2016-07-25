a29ecdd97726ae7a5c0f0798d884d32d1175b18d
ryan modeste <ryan modeste>
2016-07-25 16:38:55 +0100

    added all functions to be used in the game

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,48 +1,60 @@
-console.log("JS file is connected to HTML! Woo!")
+var cards = ['queen', 'queen', 'king', 'king'];
 
-//Cards that will be used in the game
-var cards = ['queen', 'queen', 'king', 'king']
-
+// represents the cards in play
 var cardsInPlay = [];
 
-/*if (cards[0] === cards[1]) {
-	alert('You found a match');
-}
-else {
-	alert('Sorry, try again')	
-};*/
-
+// find the board and set it to a variable
 var board = document.getElementById('game-board');
-// function will create board
+// function that will create your board
 function createBoard() {
+  // loop through your cards array to create card elements for your board
+  for (var i=0; i<cards.length; i++) {
+    // create a div element which will be used as a card
+    var newCardElement = document.createElement('div');
+    // add a class to the card element which will help link styling
+    newCardElement.className = 'card';
+		// this will set the card's 'data-card' to be the element of the array
+    // data- attributes are meant to store data about an element that is not tied to a style.
+    // i.e. "king"
+    newCardElement.setAttribute('data-card', cards[i]);
+		// when a card is clicked the function isTwoCards will be executed
+    newCardElement.addEventListener('click', isTwoCards);
 
-for(var i=0; i<cards.length; i++){
-	
-	var newCard = document.createElement('div');
-	
-	newCard.className = 'card';
-	board.appendChild(newCard);
-		
-	}
+    // append the card to the board
+    board.appendChild(newCardElement);
+    board.appendChild(newCardElement);
+  }
+
 };
 
-creatBoard();
-
-
-
-/*var createBoard = function(numCards) {
-	for(var i=1; i<= numCards; i++) {
-	var newCard = document.getElementById('div');
-	newCard.className = "card";
-	gameBoard.appendChild(newCard);
+function isTwoCards() {
+  // add card to array of cards being viewed
+  
+  cardsInPlay.push(this.getAttribute('data-card'));
+	// show the card's image
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png'>"; // king
+	} else {
+		this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>"; //queen
 	}
-};
+  
+  if (cardsInPlay.length === 2) {
+    
+    isMatch(cardsInPlay);
+    
+    cardsInPlay = [];
+  }
+}
 
-createBoard(4);
+function isMatch(cards) {
+  // alert winning message
+  if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
 
+  }
+}
 
-/*
-
- Assuming your memory card game consists of four cards, create a for loop that makes an HTML element for each card. Each HTML element should be a div. Each element should also have the class card (this will help when you add CSS).
-
-*/
+createBoard();
